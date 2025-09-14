f8f103d106806f9433e8602033bc6cc42ba4c3eb
Cesarus85 <145186748+Cesarus85@users.noreply.github.com>
2025-09-14 18:59:35 +0200

    Update gun.js

--- src/gun.js
+++ src/gun.js
@@ -23,7 +23,7 @@ export class GunSystem {
     this.tmpV = new THREE.Vector3();
     this.tmpUp = new THREE.Vector3(0,1,0);
 
-    // Trigger-Status von beiden Controllern
+    // Trigger-Status
     this.trigPressed = [false, false];
     this.controllers = [renderer.xr.getController(0), renderer.xr.getController(1)];
     this.controllers.forEach((c, i) => {
@@ -31,12 +31,13 @@ export class GunSystem {
       c.addEventListener('selectend',   ()=>{ this.trigPressed[i] = false; });
     });
 
-    // Turret selbst nicht hittbar
+    // Eigene Geometrien ignorieren
     this.markIgnore(turret.root);
+    if (muzzleFx?.sprite) this.markIgnore(muzzleFx.sprite);
   }
 
   markIgnore(obj) {
-    obj.traverse(o => { o.userData.ignoreHit = true; });
+    obj.traverse ? obj.traverse(o => { o.userData.ignoreHit = true; }) : (obj.userData.ignoreHit = true);
   }
 
   isFiring() {
@@ -46,28 +47,30 @@ export class GunSystem {
   pulseShot() {
     const amp = CONFIG.haptics.shotAmp, ms = CONFIG.haptics.shotMs;
     this.controllers.forEach(c => {
-      const gp = c?.gamepad;
-      const h = gp?.hapticActuators?.[0];
-      if (h && h.playEffect) h.playEffect('dual-rumble', { startDelay: 0, duration: ms, weakMagnitude: amp, strongMagnitude: amp });
-      else if (h && h.pulse) h.pulse(amp, ms);
+      const h = c?.gamepad?.hapticActuators?.[0];
+      if (h?.playEffect) h.playEffect('dual-rumble', { startDelay: 0, duration: ms, weakMagnitude: amp, strongMagnitude: amp });
+      else if (h?.pulse) h.pulse(amp, ms);
     });
+  }
+
+  getActiveCamera() {
+    return this.renderer.xr.isPresenting ? this.renderer.xr.getCamera(this.camera) : this.camera;
   }
 
   update(dt) {
     this.timeSinceShot += dt;
     if (this.coolDelay > 0) this.coolDelay -= dt;
 
-    // Cooldown / Overheat-Reset
+    // Cooldown/Overheat
     if (this.coolDelay <= 0) {
-      const cool = CONFIG.fire.heatCoolRate * dt;
-      this.heat = Math.max(0, this.heat - cool);
+      this.heat = Math.max(0, this.heat - CONFIG.fire.heatCoolRate * dt);
       if (this.overheated && this.heat <= CONFIG.fire.overheatThreshold * 0.35) {
-        this.overheated = false; // wieder einsatzbereit
+        this.overheated = false;
       }
     }
 
     // UI
-    if (this.heatUI) this.heatUI.setHeat01(this.heat / CONFIG.fire.overheatThreshold);
+    this.heatUI?.setHeat01(this.heat / CONFIG.fire.overheatThreshold);
 
     // Schießen
     if (!this.overheated && this.isFiring() && this.timeSinceShot >= this.shotInterval) {
@@ -78,7 +81,6 @@ export class GunSystem {
       if (this.heat >= CONFIG.fire.overheatThreshold) {
         this.overheated = true;
         this.audio?.playOverheat();
-        // kräftiges Haptiksignal
         const amp = CONFIG.haptics.overheatAmp, ms = CONFIG.haptics.overheatMs;
         this.controllers.forEach(c=>{
           const h = c?.gamepad?.hapticActuators?.[0];
@@ -90,16 +92,18 @@ export class GunSystem {
   }
 
   fireOneShot() {
-    // Mündungs-Pos + Richtung aus Pitch-Pivot ableiten
+    // Kamera für Sprite-Raycasts setzen (fix für deinen Fehler)
+    this.raycaster.camera = this.getActiveCamera();
+
+    // Mündungs-Pos + Richtung
     const pivot = this.turret.pitchPivot;
     const muzzleOffset = CONFIG.fire.muzzleOffset;
     const dir = new THREE.Vector3(0,0,-1).applyQuaternion(pivot.getWorldQuaternion(this.tmpQ)).normalize();
-    const origin = pivot.getWorldPosition(this.tmpV.clone()).add(dir.clone().multiplyScalar(muzzleOffset));
+    const origin = pivot.getWorldPosition(this.tmpV.set(0,0,0)).add(dir.clone().multiplyScalar(muzzleOffset));
 
     // Spread
     const spreadRad = THREE.MathUtils.degToRad(CONFIG.fire.spreadDeg);
     if (spreadRad > 0) {
-      // zufällige kleine Rotation um eine Achse senkrecht zu dir
       const axis = new THREE.Vector3().crossVectors(dir, this.tmpUp).normalize();
       const axis2 = new THREE.Vector3().crossVectors(dir, axis).normalize();
       const a = (Math.random()-0.5) * spreadRad;
@@ -115,25 +119,24 @@ export class GunSystem {
     const hits = this.raycaster.intersectObjects(this.scene.children, true);
     let hit = null;
     for (const h of hits) {
-      if (h.object.userData?.ignoreHit) continue; // Turret etc. ignorieren
+      if (h.object.userData?.ignoreHit) continue;
       hit = h; break;
     }
 
-    // FX + Audio + Rückstoß
+    // FX/Audio/Haptik
     this.muzzleFx?.trigger(CONFIG.fire.muzzleFlashMs);
     this.audio?.playShot();
     this.pulseShot();
 
     if (hit) {
-      this.hitFx?.spawnAt(hit.point, hit.face?.normal || dir.clone().negate());
-      // TODO: Schaden später über hit.object.userData?.damageable / callback
+      const n = hit.face?.normal ? hit.face.normal.clone().applyNormalMatrix(new THREE.Matrix3().getNormalMatrix(hit.object.matrixWorld)) : dir.clone().negate();
+      this.hitFx?.spawnAt(hit.point, n);
+      // TODO: Schaden an Gegner
     }
 
-    // leichter Rückstoß (Pitch hoch)
-    if (typeof this.turret.setTargetAngles === 'function') {
-      const yawNow = this.turret._targetYaw ?? this.turret.yawPivot.rotation.y;
-      const pitchNow = this.turret._targetPitch ?? this.turret.pitchPivot.rotation.x;
-      this.turret.setTargetAngles(yawNow, pitchNow - CONFIG.fire.recoilPitch);
-    }
+    // leichter Rückstoß
+    const yawNow = this.turret._targetYaw ?? this.turret.yawPivot.rotation.y;
+    const pitchNow = this.turret._targetPitch ?? this.turret.pitchPivot.rotation.x;
+    this.turret.setTargetAngles(yawNow, pitchNow - CONFIG.fire.recoilPitch);
   }
 }
