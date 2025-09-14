648a24de32fc59ab0e162f2b42a6c881190bec78
Cesarus85 <145186748+Cesarus85@users.noreply.github.com>
2025-09-14 20:06:37 +0200

    Update gun.js

--- src/gun.js
+++ src/gun.js
@@ -91,52 +91,58 @@ export class GunSystem {
     }
   }
 
-  fireOneShot() {
-    // Kamera für Sprite-Raycasts setzen (fix für deinen Fehler)
-    this.raycaster.camera = this.getActiveCamera();
+fireOneShot() {
+  // Kamera für Sprite-Raycasts setzen (wichtig für Sprites)
+  this.raycaster.camera = this.getActiveCamera();
 
-    // Mündungs-Pos + Richtung
-    const pivot = this.turret.pitchPivot;
-    const muzzleOffset = CONFIG.fire.muzzleOffset;
-    const dir = new THREE.Vector3(0,0,-1).applyQuaternion(pivot.getWorldQuaternion(this.tmpQ)).normalize();
-    const origin = pivot.getWorldPosition(this.tmpV.set(0,0,0)).add(dir.clone().multiplyScalar(muzzleOffset));
+  // Mündungs-Pos + Richtung
+  const pivot = this.turret.pitchPivot;
+  const muzzleOffset = CONFIG.fire.muzzleOffset;
+  const dir = new THREE.Vector3(0,0,-1).applyQuaternion(pivot.getWorldQuaternion(this.tmpQ)).normalize();
+  const origin = pivot.getWorldPosition(this.tmpV.set(0,0,0)).add(dir.clone().multiplyScalar(muzzleOffset));
 
-    // Spread
-    const spreadRad = THREE.MathUtils.degToRad(CONFIG.fire.spreadDeg);
-    if (spreadRad > 0) {
-      const axis = new THREE.Vector3().crossVectors(dir, this.tmpUp).normalize();
-      const axis2 = new THREE.Vector3().crossVectors(dir, axis).normalize();
-      const a = (Math.random()-0.5) * spreadRad;
-      const b = (Math.random()-0.5) * spreadRad;
-      dir.add(axis.multiplyScalar(a)).add(axis2.multiplyScalar(b)).normalize();
-    }
-
-    // Raycast
-    this.raycaster.ray.origin.copy(origin);
-    this.raycaster.ray.direction.copy(dir);
-    this.raycaster.far = CONFIG.fire.range;
-
-    const hits = this.raycaster.intersectObjects(this.scene.children, true);
-    let hit = null;
-    for (const h of hits) {
-      if (h.object.userData?.ignoreHit) continue;
-      hit = h; break;
-    }
-
-    // FX/Audio/Haptik
-    this.muzzleFx?.trigger(CONFIG.fire.muzzleFlashMs);
-    this.audio?.playShot();
-    this.pulseShot();
-
-    if (hit) {
-      const n = hit.face?.normal ? hit.face.normal.clone().applyNormalMatrix(new THREE.Matrix3().getNormalMatrix(hit.object.matrixWorld)) : dir.clone().negate();
-      this.hitFx?.spawnAt(hit.point, n);
-      // TODO: Schaden an Gegner
-    }
-
-    // leichter Rückstoß
-    const yawNow = this.turret._targetYaw ?? this.turret.yawPivot.rotation.y;
-    const pitchNow = this.turret._targetPitch ?? this.turret.pitchPivot.rotation.x;
-    this.turret.setTargetAngles(yawNow, pitchNow - CONFIG.fire.recoilPitch);
+  // Spread
+  const spreadRad = THREE.MathUtils.degToRad(CONFIG.fire.spreadDeg);
+  if (spreadRad > 0) {
+    const axis = new THREE.Vector3().crossVectors(dir, this.tmpUp).normalize();
+    const axis2 = new THREE.Vector3().crossVectors(dir, axis).normalize();
+    const a = (Math.random()-0.5) * spreadRad;
+    const b = (Math.random()-0.5) * spreadRad;
+    dir.add(axis.multiplyScalar(a)).add(axis2.multiplyScalar(b)).normalize();
   }
+
+  // Raycast
+  this.raycaster.ray.origin.copy(origin);
+  this.raycaster.ray.direction.copy(dir);
+  this.raycaster.far = CONFIG.fire.range;
+
+  const hits = this.raycaster.intersectObjects(this.scene.children, true);
+  let hit = null;
+  for (const h of hits) {
+    if (h.object.userData?.ignoreHit) continue;
+    hit = h; break;
+  }
+
+  // FX/Audio/Haptik
+  this.muzzleFx?.trigger(CONFIG.fire.muzzleFlashMs);
+  this.audio?.playShot();
+  this.pulseShot();
+
+  if (hit) {
+    // visuelles Feedback
+    const n = hit.face?.normal ? hit.face.normal.clone().applyNormalMatrix(new THREE.Matrix3().getNormalMatrix(hit.object.matrixWorld)) : dir.clone().negate();
+    this.hitFx?.spawnAt(hit.point, n);
+
+    // ❗ Schaden, wenn ein Enemy getroffen wurde
+    const enemy = hit.object.userData?.enemy;
+    if (enemy && typeof enemy.takeDamage === 'function') {
+      enemy.takeDamage(CONFIG.fire.damage);
+    }
+  }
+
+  // leichter Rückstoß
+  const yawNow = this.turret._targetYaw ?? this.turret.yawPivot.rotation.y;
+  const pitchNow = this.turret._targetPitch ?? this.turret.pitchPivot.rotation.x;
+  this.turret.setTargetAngles(yawNow, pitchNow - CONFIG.fire.recoilPitch);
+}
 }
